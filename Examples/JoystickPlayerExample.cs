98385d02086b9adcb26064437692d5e31d76a6c9
thanhthai18 <hathanhthai18@gmail.com>
2021-11-18 23:46:46 +0700

    First commit

---
+++ Examples/JoystickPlayerExample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        Vector2 direction = new Vector2( variableJoystick.Horizontal, variableJoystick.Vertical);
+        rb.AddForce(direction * speed * Time.fixedDeltaTime);
+    }
+}
